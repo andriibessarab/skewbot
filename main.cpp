ffe5410594364920dbf0d85ac97b09e3ec598a16
IdkWhatUsername2do <szlgm2018@gmail.com>
2025-10-02 20:12:18 -0400

    Standard

--- main.cpp
+++ main.cpp
@@ -4,12 +4,16 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+struct skewb_state;
 
 
 std::ofstream output("Output.txt", std::ios::app);
 std::vector<std::string> history = {};
+
 int depth = 0;
+
 int main() {
+
     if (!output) {
         std::cout << "Error opening file" << std::endl;
         return 1;
@@ -45,32 +49,35 @@ struct skewb_state {
 
     std::vector<int> corner_orientations;
 
+
+
     skewb_state() : center_colors{W, G, O, B, R, Y},
                     corner_permutations{UFL, UBL, UBR, UFR, DFL, DBL, DBR, DFR},
                     corner_orientations{0, 0, 0, 0, 0, 0, 0, 0} {
     }
+
 
     void reset() {
         *this = skewb_state();
     }
 
 
-    bool isEqual(const skewb_state state) const {
-        return this->center_colors == state.center_colors && this->corner_permutations == state.corner_permutations && this->corner_orientations == state.corner_orientations;
-
-
+    bool is_equal(const skewb_state state) const {
+        return this->center_colors == state.center_colors && this->corner_permutations == state.corner_permutations &&
+               this->corner_orientations == state.corner_orientations;
     }
-    void Right(int turn) {
 
+    void r_move(int turn) {
     }
-    void Left(int turn) {
 
+    void l_move(int turn) {
     }
-    void Back(int turn) {//inv
 
+    void b_move(int turn) {
+        //inv
     }
-    void Up(int turn) {
 
+    void u_move(int turn) {
     }
 };
 
@@ -78,8 +85,4 @@ std::queue<skewb_state> q;
 
 void BFS(int depth, skewb_state CurrentState, std::vector<std::string> history) {
 
-
-
-
 }
-
