4cbaa7273db1361111c3d1684052161d7ac9c9de
andriibessarab <andriibessarab@gmail.com>
2025-11-04 23:11:20 -0500

    Move frontend to client folder

--- client/src/SendData.tsx
+++ client/src/SendData.tsx
@@ -1,15 +1,18 @@
 import { GlobalSavedValues } from "./DisplaySides"
 
 function SendDataButton(){
-    return (<button style={{position:"absolute",right:"50px", top:"50px", width:"10vw",height:"10vh"}}onClick={SendOverData}>Send Data!</button>)
+    // Now it calls the correct function
+    return (<button style={{position:"absolute",right:"50px", top:"50px", width:"10vw",height:"10vh"}} onClick={sendData}>Send Data!</button>)
 }
+
 function SendOverData(){
     console.log(GlobalSavedValues.flat().join(""));
 }
 
 // This is the correct client code, which now requires a server-side CORS fix.
 async function sendData() {
-    const url = "http://10.39.43.184:5001/api/send_state";
+    const url = "/api/send_state";
+
     const payload = {
         "state": GlobalSavedValues.flat().join("")
     };
@@ -18,15 +21,13 @@ async function sendData() {
         const response = await fetch(url, {
             method: "POST",
             headers: {
-                "Content-Type": "application/json", // Server MUST have this
+                "Content-Type": "application/json",
             },
             body: JSON.stringify(payload),
         });
-
-        // ... (rest of the logic)
     } catch (error) {
         console.error("Error sending data:", error);
     }
 }
   
-export default SendDataButton
+export default SendDataButton;
