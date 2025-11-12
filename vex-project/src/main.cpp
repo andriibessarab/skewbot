6c69c75dee4c97e9ff10f9e9fa785aa8a6d40a05
IdkWhatUsername2do <szlgm2018@gmail.com>
2025-11-12 00:20:53 -0500

    Added FindSolution Function, DID NOT TRY AND TEST AT ALL MAKE SURE TO DO BEFORE MOVING ON

--- vex-project/src/main.cpp
+++ vex-project/src/main.cpp
@@ -1,8 +1,8 @@
 //----------------------------------------------------------------------------
-//                                                                          
-//  Module:       main.cpp                                                    
-//  Description:  The main entrypoint                                                                                   
-//                                                                          
+//
+//  Module:       main.cpp
+//  Description:  The main entrypoint
+//
 //----------------------------------------------------------------------------
 
 // --- Info ---
@@ -15,6 +15,8 @@
 #include "robot_config.h"
 #include "state_converter.h"
 
+
+
 using namespace vex;
 
 // --- Constants ---
@@ -23,6 +25,7 @@ const int MAX_BUFFER_SIZE = 100;
 // --- Local Functions Prototypes ---
 void print_status(std::string status);
 std::string get_state_from_serial();
+void FindSolutions(std::string CubeState);
 
 /////////////////////////////// MAIN PROGRAM ///////////////////////////////
 int main()
@@ -51,8 +54,6 @@ int main()
     // print_status("Thinking...");
 
     // // Obtain solution
-
-
 
     // wait(2, seconds);
     // print_status("Ready!");
@@ -95,4 +96,93 @@ std::string get_state_from_serial()
         }
         wait(20, msec);
     }
+}
+
+bool CheckOrientation(const std::string &Orientation);
+void FindSolutions(const std::string &CubeState){
+    const int StateSize = 9;
+    std::string State[StateSize];
+    int PrevSubIndex = 0;
+    int IndexToPush = 0;
+    bool found = false;
+    const char Centers[6] = {'W', 'G', 'O', 'B', 'R', 'Y'};
+    std::string Solution;
+    const std::array<std::string, 8> Corners = {"UFR", "UFL", "UBL", "UBR", "DFR", "DFL", "DBL", "DBR"};
+    const char Moves[8] = {'L', 'l', 'R', 'r', 'U', 'u', 'F', 'f'};
+    for (int i = 0; i < CubeState.length(); i++){
+        if (CubeState[i] == ' '){
+            State[IndexToPush] = CubeState.substr(PrevSubIndex, i-PrevSubIndex);
+            PrevSubIndex = i+1;
+            IndexToPush++;
+            if (IndexToPush == 8){
+                State[8] = CubeState.substr(PrevSubIndex);
+                //Get out of for loop cz no break...
+                i = CubeState.length();
+            }
+        }
+    }
+    FILE* file = fopen("SkewSolutions.bin","rb");
+    if (!file){
+        Brain.Screen.print("Failed To Open SkewSolutions.bin");
+        return;
+    }
+    while (!found){
+        uint64_t CurState = 0b0;
+        std::string centers;
+        size_t Read1 = fread(&CurState, sizeof(CurState), 1, file);
+        uint8_t current = 0b0;
+        bool IsEliminated = false;
+        if (Read1 != 1){
+            found = true;
+        }else{
+        for (int i = 0; i < 6; i++) {
+            current = (CurState >> (40 + (i * 3))) & 0b111;
+            if (Centers[current] != State[0][i]){
+                fseek(file,32,SEEK_CUR);
+                IsEliminated = true;
+                //get out of for loop cz no break...
+                i = 6;
+            }
+        }
+        if(IsEliminated){
+            continue;
+        }
+        for (int i = 0; i < 8; i++) {
+            current = (CurState >> (16 + (i * 3))) & 0b111;
+            if(Corners[current] != State[1+i]){
+                fseek(file,32,SEEK_CUR);
+                IsEliminated = true;
+                //get out of for loop cz no break...
+                i = 8;
+            }
+        }
+        if (IsEliminated){
+            continue;
+        }
+        std::string Orienation;
+        for (int i = 0; i < 8; i++) {
+            current = (CurState >> (2 * i)) & 0b11;
+            //convert 0,1,2,3,4,5 -> '0','1'...etc
+            Orienation += '0' + current;
+        }
+        if(CheckOrientation(Orienation)){
+            found = true;
+            uint32_t EncodedSolution = 0b0;
+            size_t ReadSuccessful = fread(&EncodedSolution, sizeof(EncodedSolution), 1, file);
+            if(ReadSuccessful == 1){
+                for (int i = 0; i < 11; i++) {
+                current = (EncodedSolution >> (i * 3)) & 0b111;
+                Solution += Moves[current];
+        }
+        }
+    }else{
+        fseek(file,32,SEEK_CUR);
+    }
+    
+    
+
+
+   
+}
+}
 }
